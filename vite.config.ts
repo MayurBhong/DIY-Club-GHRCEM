39c622f332af0b45fe8615af45e933af7a448ad3
Mayur Bhong <bhongmayur5@gmail.com>
2025-04-07 00:36:56 +0530

    2 commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/DIY-Club-GHRCEM/",
+  base: "/DIY-Club-GHRCEM/",
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  server: {
+    host: '0.0.0.0',   // Allow access from your network
+    port: 5173,        // You can change this if needed
   },
 });
