f2498d7542b82ecb9ed1323bddd05e215cfc00ff
Mayur Bhong <bhongmayur5@gmail.com>
2025-04-06 20:08:52 +0530

    sec commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/DIY-Club-GHRCEM",
+  base:"/DIY-Club-GHRCEM/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
